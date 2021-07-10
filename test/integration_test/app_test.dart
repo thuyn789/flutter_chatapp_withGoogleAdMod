3defe55dfaa7cdca6b205b75287fa791a1409638
thuyn789 <thuynh0135@gmail.com>
2021-07-10 00:37:45 -0400

    Updated integration test

--- test/integration_test/app_test.dart
+++ test/integration_test/app_test.dart
@@ -3,12 +3,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:chatapp_admod/main.dart';
 
-
 void main() {
   group('Testing App Performance Tests', () {
     final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
-    as IntegrationTestWidgetsFlutterBinding;
+        as IntegrationTestWidgetsFlutterBinding;
 
     binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+
+    testWidgets('Scrolling test', (tester) async {
+      await tester.pumpWidget(ChatApp());
+
+      final listFinder = find.byType(ListView);
+
+      await binding.watchPerformance(() async {
+        await tester.fling(listFinder, Offset(0, -500), 10000);
+        await tester.pumpAndSettle();
+
+        await tester.fling(listFinder, Offset(0, 500), 10000);
+        await tester.pumpAndSettle();
+      }, reportKey: 'scrolling_summary');
+    });
   });
 }
